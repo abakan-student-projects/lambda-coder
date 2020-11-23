38fd653ec4a66cf920d730f0315da85f53d68aeb
v.shalginov <slava8427@mail.ru>
2020-11-23 10:53:19 +0700

    inital commit

--- backend/hasura/hasura-dev.sh
+++ backend/hasura/hasura-dev.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-docker run -d -p 8080:8080 \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://lambda:lambda@host.docker.internal:5432/lambda_coder \
+docker run -p 8080:8080 \
+       -e HASURA_GRAPHQL_DATABASE_URL=postgres://lambda:lambda@192.168.1.142:5432/lambda_coder \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=false \
        -e HASURA_GRAPHQL_DEV_MODE=true \
        -e HASURA_GRAPHQL_ADMIN_SECRET=devsecret \
